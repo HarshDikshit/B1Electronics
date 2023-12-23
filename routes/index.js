cc779ad662e8e948cef10652e80bdcaa46f844b1
HarshDikshit <93562022+HarshDikshit@users.noreply.github.com>
2023-12-23 21:45:26 +0530

    2nd commit

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,56 @@
 var express = require('express');
 var router = express.Router();
 const userModel=require("./users");
+const session=require('express-session');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render("index", {name: "himanshu"});
 });
 
-router.get('/createduser', async function(req, res, next) {
-  let createdUser = await userModel.create({
-   username : "harsh",
-   password : "aman"
-  });
-  res.send(createdUser);
- });
+router.use(session({
+  resave: false,
+  saveUnitialized: false,
+  secret:"ghvvhvhbbj"
+}));
+
+//admin route
+router.get('/admin', function(req, res, next) {
+
+   if(req.session.username){
+   
+    req.session.destroy();
+
+    //linking admin page
+    res.render("admin");
+   }else{
+    res.redirect('/login');
+   }
+  
+});
+
+
+//login route
+router.get('/login', function(req, res, next) {
+  res.render("login", {name: "himanshu"});
+});
+
+
+router.post('/login', async function(req, res, next) {
+
+ if(await userModel.findOne({username : req.body.username,
+password: req.body.password})){ 
+  let user= await userModel.findOne({username : req.body.username});
+  req.session.username= user.username;
+  res.redirect('/admin');
+}else{
+ res.send("error");
+}
+
+
+});
 
 
 module.exports = router;
